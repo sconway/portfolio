202b216f5d5caa9d015219d0a07f9c688e6c70c7
Scott Conway <scoot1243@gmail.com>
2017-01-19 03:19:17 -0500

    initial commit

---
+++ js/freelancer.js
@@ -0,0 +1,56 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize Masonry Plugin
+    $(".portfolio-items").masonry({});
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+    // Moves the input label up when the field is focused on
+    $(".form-control").on("focus", function() {
+        $(this).parent().addClass("floating-label-form-group-with-value");
+    }).on("blur", function() {
+        if (!$(this).val()) {
+            $(this).parent().removeClass("floating-label-form-group-with-value");
+        }
+    });
+
+})(jQuery); // End of use strict
